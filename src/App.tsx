ca17dd0ba931fbab4da5304feb9b5b08b21d73a3
amoong <moo1323@naver.com>
2021-01-24 15:39:27 +0900

    Understanding useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+type Action = { type: typeof INCREMENT } | { type: typeof DECREMENT };
+interface IStateProps {
+  count: number;
+}
+
+const reducer = (state: IStateProps, action: Action): IStateProps => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Sub</button>
     </div>
   );
 }
