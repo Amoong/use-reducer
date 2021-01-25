d2ba44ee0dfbd8ab7d6c645b2ac43c6ddd2519d8
amoong <moo1323@naver.com>
2021-01-25 22:22:14 +0900

    Deleting To Dos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React, { useReducer, useState } from "react";
+import { v4 } from "uuid";
 
 const ADD = "add";
+const DEL = "del";
 
-type Action = { type: typeof ADD; payload: ToDo };
+type Action =
+  | { type: typeof ADD; payload: ToDo }
+  | { type: typeof DEL; payload: string };
 
 interface ToDo {
   text: string;
+  id: string;
 }
 interface IStateProps {
   toDos: Array<ToDo>;
@@ -21,7 +26,10 @@ const reducer = (
 ): IStateProps => {
   switch (action.type) {
     case ADD:
-      return { toDos: [...state.toDos, { text: action.payload.text }] };
+      const { text, id } = action.payload;
+      return { toDos: [...state.toDos, { text, id }] };
+    case DEL:
+      return { toDos: state.toDos.filter(toDo => toDo.id !== action.payload) };
     default:
       throw new Error();
   }
@@ -33,7 +41,7 @@ function App() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch({ type: ADD, payload: { text: newToDo } });
+    dispatch({ type: ADD, payload: { text: newToDo, id: v4() } });
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,8 +55,13 @@ function App() {
         <input onChange={handleChange} type="text" />
       </form>
       <ul>
-        {state.toDos.map((item: ToDo, index: number) => (
-          <li key={index}>{item.text}</li>
+        {state.toDos.map((item: ToDo) => (
+          <li key={item.id}>
+            <span>{item.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: item.id })}>
+              ❌
+            </button>
+          </li>
         ))}
       </ul>
     </div>
